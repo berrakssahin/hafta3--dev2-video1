39d4e23339d35dcefc90db41593eba4fb594ec16
berrakssahin <112779654+berrakssahin@users.noreply.github.com>
2022-10-26 01:15:31 +0300

    Add files via upload

---
+++ classesWithAttributes/src/ProductManager.java
@@ -0,0 +1,11 @@
+public class ProductManager {
+    public void Add(Product product) {
+        System.out.println("Ürüm eklendi" + product.name);
+    }
+
+    public void Add(int id, String name,String description, int stockAmount, double price ) {
+
+    }
+
+}
+
